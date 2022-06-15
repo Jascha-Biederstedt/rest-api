8af5789c82e9ee2abaa15712694649380f6d81a7
Jascha-Biederstedt <j.biederstedt@gmx.de>
2022-06-15 12:37:53 +0200

    Set up the /trips endpoint to only receive GET requests

--- pages/api/trips/index.js
+++ pages/api/trips/index.js
@@ -1,4 +1,11 @@
 const handler = (req, res) => {
+  if (req.method !== 'GET') {
+    res.status(405).json({
+      message: 'Method not allowed',
+    });
+    return;
+  }
+
   res.status(200).json({ name: 'API Trips' });
 };
 
